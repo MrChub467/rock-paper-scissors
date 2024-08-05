3309ef2f8a0227d0d683514b4113ce19516f5640
MrChub467 <173291253+MrChub467@users.noreply.github.com>
2024-08-05 11:28:39 -0500

    Add function getComputerChoice
    This function gets a random number and converts
    that number into either rock paper of scissors.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getComputerChoice() {
+  let choice;
+  let num = Math.floor(Math.random() * 3);
+  switch (num) {
+    case 0:
+      choice = "rock";
+      break;
+    case 1:
+      choice = "paper";
+      break;
+    case 2:
+      choice = "scissors";
+      break;
+  }
+  return choice;
+}
+
+console.log(getComputerChoice());
