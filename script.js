30a78b62e36549a33d758ed4af9b62a8b786e1c1
MrChub467 <173291253+MrChub467@users.noreply.github.com>
2024-08-05 11:49:06 -0500

    Create function to get player input
    It gets the player to enter either rock paper or
    scissors or the shorthands: r p s. It gets rid of
    whitespace and lowercases the input.

--- script.js
+++ script.js
@@ -15,4 +15,30 @@ function getComputerChoice() {
   return choice;
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+  let keepGoing = true;
+  let playerChoice;
+  
+  playerChoice = prompt("Please enter rock(r), paper(p), or scissors(s)");
+
+  while(keepGoing) {
+    playerChoice = playerChoice.trim().toLowerCase();
+    if (playerChoice === "rock" ||
+        playerChoice === "r" ||
+        playerChoice === "paper" ||
+        playerChoice === "p" ||
+        playerChoice === "scissors" ||
+        playerChoice === "s") {
+          keepGoing = false
+        }
+    else {
+      playerChoice = prompt("Invalid input. Please enter rock(r), paper(p), or scissors(s)");
+    }    
+  }
+
+
+
+  return playerChoice;
+}
+
+console.log(getPlayerChoice());
